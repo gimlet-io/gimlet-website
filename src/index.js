2dc6a10f2cea0ecc4a7b88c840c4de477ff2e05d
Laszlo Fogas <laszlo@laszlo.cloud>
2020-01-24 10:14:06 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,3 @@
+import _ from 'lodash';
+import './style.css';
+require('file-loader?name=[name].[ext]!./index.html');
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          { loader: 'css-loader', options: { importLoaders: 1 } },
+          'postcss-loader',
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader',
+        ],
+      },
+    ],
+  },
+};
